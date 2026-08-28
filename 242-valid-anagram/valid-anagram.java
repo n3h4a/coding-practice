94ae8307aaabb5def5bec4792ec654d5ee3d79c9
neha <nehakaramchandani2005@gmail.com>
2026-08-28 18:05:30 +0530

    Time: 5 ms (62.40%) | Memory: 44.3 MB (82.30%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -11,6 +11,8 @@ class Solution {
         return Arrays.equals(arr1 , arr2);
         }
 */
+
+/*
                 if(s.length() != t.length()){
                     return false;
                 } else {
@@ -27,6 +29,25 @@ class Solution {
                     mapT.put(ch2 , currentcount2);
                     }
                     return mapS.equals(mapT);
+
                 }
-    }
+*/
+
+                                             if (s.length() != t.length()) {
+                                                return false;
+                                            }
+
+                                            int[] count = new int[26];
+                                            for (int i = 0; i < s.length(); i++) {
+                                                count[s.charAt(i) - 'a']++;
+                                                count[t.charAt(i) - 'a']--;
+                                            }
+
+                                            for (int val : count) {
+                                                if (val != 0) {
+                                                    return false;
+                                                }
+                                            }
+                                            return true;
+}
 }
