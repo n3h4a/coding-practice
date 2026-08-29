878e318913876a44a5619c34c9b994a8f82bc902
neha <nehakaramchandani2005@gmail.com>
2026-08-29 21:33:09 +0530

    Time: 2 ms (99.34%) | Memory: 47.4 MB (11.17%) - LeetSync

--- 1-two-sum/two-sum.java
+++ 1-two-sum/two-sum.java
@@ -39,7 +39,7 @@ class Solution {
 
 
 
-                                        HashMap <Integer , Integer> indices = new HashMap <> ();
+                        /*                HashMap <Integer , Integer> indices = new HashMap <> ();
                                         for(int i = 0 ; i < nums.length ; i++){
                                             indices.put(nums[i] , i);
                                         }
@@ -49,8 +49,24 @@ class Solution {
                                                 return new int [] {indices.get(diff) , i};
                                             }
                                         }
-                                        return new int [0];
+                                        return new int [0];              */
                                     
+
+
+         HashMap<Integer, Integer> prevMap = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            int num = nums[i];
+            int diff = target - num;
+
+            if (prevMap.containsKey(diff)) {
+                return new int[] { prevMap.get(diff), i };
+            }
+
+            prevMap.put(num, i);
+        }
+
+        return new int[] {};
 
 
     }
