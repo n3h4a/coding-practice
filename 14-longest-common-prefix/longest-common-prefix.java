f8438340ced967bd4e2e0f1c63cbeeb552ef0b81
neha <nehakaramchandani2005@gmail.com>
2026-09-03 09:53:29 +0530

    Time: 1 ms (60.49%) | Memory: 43.3 MB (50.56%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.java
+++ 14-longest-common-prefix/longest-common-prefix.java
@@ -23,14 +23,29 @@ class Solution {
         return prefix;    */
 
 
-                                for ( int i = 0; i < strs[0].length() ; i++) {
+                        /*        for ( int i = 0; i < strs[0].length() ; i++) {
                                     for(String s : strs){
                                         if(i == s.length() || s.charAt(i) != strs[0].charAt(i) ){
                                             return s.substring (0 , i);
                                         }
                                     }
                                 }
-                                return strs[0];
+                                return strs[0];        */
+
+                                                                if(strs.length == 1){
+                                                                    return strs[0];
+                                                                }else {
+
+                                                                Arrays.sort(strs);
+                                                                int N = Math.min(strs[0].length() , strs[strs.length - 1].length());
+                                                                for(int i = 0 ; i < N ; i++){
+                                                                if(strs[0].charAt(i) != strs[strs.length - 1].charAt(i)){
+                                                                    return strs[0].substring(0 ,i);
+                                                                }
+                                                                }
+                                                                }
+                                                                return strs[0];
+                                        
 
 
 
