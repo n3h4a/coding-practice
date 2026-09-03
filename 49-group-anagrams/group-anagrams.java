99f8e2d2d8b2945a8e00a4ba963be43f737f6271
neha <nehakaramchandani2005@gmail.com>
2026-09-03 21:34:45 +0530

    Time: 20 ms (12.41%) | Memory: 51.4 MB (5.07%) - LeetSync

--- 49-group-anagrams/group-anagrams.java
+++ 49-group-anagrams/group-anagrams.java
@@ -1,5 +1,7 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
+    
+    /*
         Map <String , List<String>> res = new HashMap<>();
         for(String s : strs){
             char[] chars = s.toCharArray();
@@ -8,7 +10,25 @@ class Solution {
             res.putIfAbsent(sortedS , new ArrayList<>());
             res.get(sortedS).add(s);
         }
-        return new ArrayList<>(res.values());
+        return new ArrayList<>(res.values()); */
+
+
+
+
+        Map <String , List<String>> res = new HashMap <>();
+        for(String s : strs){
+            int[] count = new int [26];
+            for(char c : s.toCharArray()){
+            count[c-'a']++;
+        }
+        String key = Arrays.toString(count);
+        res.putIfAbsent(key , new ArrayList<>());
+        res.get(key).add(s);
+        }
+        return new ArrayList<> (res.values());
+
+
+        
         
     }
 }
