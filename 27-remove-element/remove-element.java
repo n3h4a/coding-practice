96817178cfdaa278bae9bfa7e0364abcbb067ab1
neha <nehakaramchandani2005@gmail.com>
2026-09-04 14:25:31 +0530

    Time: 0 ms (100.00%) | Memory: 43.8 MB (8.93%) - LeetSync

--- 27-remove-element/remove-element.java
+++ 27-remove-element/remove-element.java
@@ -1,6 +1,6 @@
 class Solution {
     public int removeElement(int[] nums, int val) {
-        int k = 0 ;
+       /*  int k = 0 ;
         for(int i = 0 ; i < nums.length ; i++){
             if (nums[i] != val){
                 nums[k] = nums[i];
@@ -8,6 +8,22 @@ class Solution {
 
             }
         }
-        return k;
+        return k;   */
+
+
+
+
+                        int i = 0, n = nums.length;
+                        while (i < n){
+                            if (nums[i] == val ){
+                                n--;
+                                nums[i] = nums[n];
+
+                            } else {
+                                i++;
+                            }
+                        }
+                        return n;
+
     }
 }
