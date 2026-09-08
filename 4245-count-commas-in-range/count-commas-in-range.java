83724717b37d21e728791e03f6ad073340f0d80a
neha <nehakaramchandani2005@gmail.com>
2026-09-09 01:08:54 +0530

    Time: 6 ms (11.44%) | Memory: 42.7 MB (40.03%) - LeetSync

---
+++ 4245-count-commas-in-range/count-commas-in-range.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int countCommas(int n) {
+        int count = 0;
+        while ( n > 999) {
+            count++;
+            n--;
+        }
+        return count;
+    }
+}
