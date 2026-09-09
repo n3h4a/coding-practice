4fec318cf895a313ba96a0df3f2316bb64b9d3dc
neha <nehakaramchandani2005@gmail.com>
2026-09-10 00:57:32 +0530

    Time: 4 ms (98.64%) | Memory: 47.6 MB (85.18%) - LeetSync

--- 680-valid-palindrome-ii/valid-palindrome-ii.java
+++ 680-valid-palindrome-ii/valid-palindrome-ii.java
@@ -1,6 +1,6 @@
 class Solution {
     public boolean validPalindrome(String s) {
-    int left = 0;
+    /* int left = 0;
     int right = s.length() - 1;
     while(left < right){
         char leftChar = s.charAt(left);
@@ -29,6 +29,34 @@ public boolean isPalindrome(String s, int left, int right){
         right--;
     }
 
-    return true;
+    return true;   */ 
+
+
+
+        int l = 0, r = s.length() - 1;
+
+        while (l < r) {
+            if (s.charAt(l) != s.charAt(r)) {
+                return isPalindrome(s, l + 1, r) ||
+                       isPalindrome(s, l, r - 1);
+            }
+            l++;
+            r--;
+        }
+
+        return true;
+    }
+
+    private boolean isPalindrome(String s, int l, int r) {
+        while (l < r) {
+            if (s.charAt(l) != s.charAt(r)) {
+                return false;
+            }
+            l++;
+            r--;
+        }
+        return true;
+
+
 }
 }
