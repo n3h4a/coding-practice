aa900b8a5d0f1e0fd11003e6fa0bc4949c403c82
neha <nehakaramchandani2005@gmail.com>
2026-09-09 20:01:52 +0530

    Time: 1 ms (99.36%) | Memory: 42.5 MB (85.26%) - LeetSync

--- 4248-count-commas-in-range-ii/count-commas-in-range-ii.java
+++ 4248-count-commas-in-range-ii/count-commas-in-range-ii.java
@@ -1,6 +1,6 @@
 class Solution {
     public long countCommas(long n) {
-        long totalCommas = 0;
+         /* long totalCommas = 0;
         totalCommas += Math.max(0L, n - 1000L + 1);
         totalCommas += Math.max(0L, n - 1000000L + 1);
         totalCommas += Math.max(0L, n - 1000000000L + 1);
@@ -8,6 +8,25 @@ class Solution {
         totalCommas += Math.max(0L, n - 1000000000000000L + 1);
         totalCommas += Math.max(0L, n - 1000000000000000000L + 1);
 
-        return totalCommas;
+        return totalCommas;   */
+
+
+                    long result = 0;
+                    long lower = 1000;
+                    long commas =  1;
+                    while( lower <= n){
+                        long upper = (lower * 1000) - 1;
+                        if( upper > n ) {
+                            upper = n;
+                        }
+                        long countNos = upper - lower + 1;
+                        result += (countNos * commas);
+                        lower *= 1000;
+                        commas++;
+                    }
+                    return result;
+
+
+
     }
 }
