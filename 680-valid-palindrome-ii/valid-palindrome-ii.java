15197d9322e90c00b5405efec1829939a268efc7
neha <nehakaramchandani2005@gmail.com>
2026-09-09 22:17:14 +0530

    Time: 14 ms (6.15%) | Memory: 47.7 MB (68.54%) - LeetSync

--- 680-valid-palindrome-ii/valid-palindrome-ii.java
+++ 680-valid-palindrome-ii/valid-palindrome-ii.java
@@ -13,14 +13,22 @@ class Solution {
         }
     } return true;
     }
-    public boolean isPalindrome(String s, int left, int right){
+
+
+public boolean isPalindrome(String s, int left, int right){
+
     while(left < right){
-        if(s.charAt(left) != s.charAt(right)){
+
+        if(Character.toLowerCase(s.charAt(left)) != 
+           Character.toLowerCase(s.charAt(right))){
+
             return false;
-            }
-            left++;
-            right--;
         }
-        return true;
+
+        left++;
+        right--;
     }
-    }
+
+    return true;
+}
+}
